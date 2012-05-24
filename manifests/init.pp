e573fbd5d22bde2f96fb459b53e19bae1306025d
Evan Stachowiak <evan.stachowiak@chloeandisabel.com>
2012-05-24 16:49:50 -0400

    Initial refactor.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,50 +1,12 @@
-# Class: s3fs
-#
-# This module manages vladgh-s3fs
-#
-# Parameters:
-#
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
-#
-#  # S3FS
-#  s3fs::mount {'Testing':
-#    bucket      => 'testvgh1',
-#    mount_point => '/srv/testvgh1',
-#    uid         => '1001',
-#    gid         => '1001',
-#  }
-#  ## S3FS
-#  s3fs::mount {'Testvgh':
-#    bucket      => 'testvgh',
-#    mount_point => '/srv/testvgh2',
-#    default_acl => 'public-read',
-#  }
-#
+import "classes/*.pp"
+import "defines/*.pp"
+
+# Module to build and populate s3fs mounts:
 class s3fs {
 
-  require s3fs::params
+  include s3fs::build
 
-  $aws_access_key_id     = hiera('aws_access_key_id')
-  $aws_secret_access_key = hiera('aws_secret_access_key')
-  $credentials           = inline_template("<%= aws_access_key_id %>:<%= aws_secret_access_key %>")
-
-  package{'S3FS Package':
-    ensure => present,
-    name   => $s3fs::params::s3fs_package,
-  }
-
-  file{'S3FS Credentials':
-    ensure  => present,
-    path    => $s3fs::params::credentials_file,
-    content => $credentials,
-    require => Package ['S3FS Package'],
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0640',
-  }
+  # Define directory under which all s3fs mounts will be done:
+  s3fs::root { "/cloud": }
 
 }
