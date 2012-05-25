76c8299ad046972f9662ec6f0c827076c829c3c9
Puppet server <sysadmin@chloeandisabel.com>
2012-05-24 21:53:06 -0400

    moved config into init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -31,7 +31,8 @@ class s3fs (
   $version               = $s3fs::params::version,
   $download_url          = $s3fs::params::download_url,
   $aws_access_key_id     = hiera('aws_access_key_id'),
-  $aws_secret_access_key = hiera('aws_secret_access_key')
+  $aws_secret_access_key = hiera('aws_secret_access_key'),
+  $credentials_file      = $s3fs::params::credentials_file
 ) inherits s3fs::params {
 
   Class['s3fs::dependencies'] -> Class['s3fs']
@@ -44,17 +45,27 @@ class s3fs (
   }
 */
 
+  $credentials = inline_template("<%= aws_access_key_id %>:<%= aws_secret_access_key %>")
+
+  file{ 's3fs_credentials':
+    ensure  => $ensure,
+    path    => $credentials_file,
+    content => $credentials,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+  }
+
   Exec['s3fs_tar_gz'] ~> Exec['s3fs_extract'] ~> Exec['s3fs_configure'] ~> Exec['s3fs_make'] ~> Exec['s3fs_install']
 
   # Distribute s3fs source from within module to control version (could
   # also download from Google directly):
   exec { 's3fs_tar_gz':
-    creates   => "${source_dir}/s3fs-${version}.tar.gz",
     command   => "curl -o ${source_dir}/s3fs-${version}.tar.gz ${download_url}/s3fs-${version}.tar.gz",
     logoutput => true,
     timeout   => 300,
     path      => '/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/local/sbin',
-    unless    => 'which s3fs && s3fs --version | grep ${version}'
+    unless    => "which s3fs && s3fs --version | grep ${version}",
   }
   
   # Extract s3fs source:
